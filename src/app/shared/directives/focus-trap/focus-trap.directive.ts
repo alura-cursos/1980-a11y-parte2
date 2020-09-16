66888a6efe29d0f51bc49eb28b8a4d200f4fb72c
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-09-16 15:57:12 +0100

    video4.2

---
+++ src/app/shared/directives/focus-trap/focus-trap.directive.ts
@@ -0,0 +1,21 @@
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appFocusTrap]'
+})
+export class FocusTrapDirective implements AfterViewInit {
+
+  private firstFocusableElement: HTMLElement = null;
+  private lastFocusableElement: HTMLElement = null;
+
+  constructor(private elementRef: ElementRef<any>) {}
+
+  public ngAfterViewInit(): void {
+    const focusableElements = this.elementRef
+      .nativeElement
+      .querySelectorAll('[tabindex]:not([tabindex="-1"]), a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]), select:not([disabled])');
+    this.firstFocusableElement = focusableElements[0];
+    this.lastFocusableElement = focusableElements[focusableElements - 1];
+    this.firstFocusableElement.focus();
+  }
+}
