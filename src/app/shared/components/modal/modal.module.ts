efeceb6e1962cb9f8711dcc925e1e2b87f0ee7a7
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2020-09-16 16:42:03 +0100

    video4.5

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FocusBackModule } from '../../directives/focus-back/focus-back.module';
 import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.service';
@@ -9,7 +10,8 @@ import { ModalService } from './services/modal.service';
   declarations: [ModalComponent],
   imports: [
     CommonModule,
-    FocusTrapModule
+    FocusTrapModule,
+    FocusBackModule
   ],
   exports: [ModalComponent],
   providers: [ModalService]
